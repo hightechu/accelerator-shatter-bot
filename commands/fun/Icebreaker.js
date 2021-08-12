3688bb10c9d1509e0cbe579fdbf88a9a7318d424
Jeff Pezo <jagmansidhu1@gmail.com>
2021-08-12 16:46:20 -0700

    Shatter

--- commands/fun/Icebreaker.js
+++ commands/fun/Icebreaker.js
@@ -1,5 +1,5 @@
 function random(message){
-    var messages = ['What’s the best trip (traveling wise) you ever had?', 'What is your favorite icecream', 'What would you do if you had 17 billion dollars'];
+    var messages = require('../Icebreaker.json')
     var rnd = Math.floor(Math.random() * messages.length);
 
     message.channel.send(messages[rnd]);
